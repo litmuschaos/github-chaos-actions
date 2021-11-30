c3eae525d0439fc3ab8a7b1f51eaace0b27cca29
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-11-30 16:10:52 +0530

    Minor enhancement in secrets setup
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,9 @@ mkdir -p $HOME/go/src/github.com/litmuschaos
 cd ${GOPATH}/src/github.com/litmuschaos/
 dir=${GOPATH}/src/github.com/litmuschaos/chaos-ci-lib
 
-if [[ ! -z $AWS_ACCESS_KEY_ID ]] && [[ ! -z $AWS_SECRET_ACCESS_KEY ]] && [[ ! -z $AWS_REGION ]]
+if [[ ! -z $AWS_ACCESS_KEY_ID ]] && [[ ! -z $AWS_SECRET_ACCESS_KEY ]] && [[ ! -z $AWS_DEFAULT_REGION ]]
 then 
-  aws configure set default.region ${AWS_REGION}
+  aws configure set default.region ${AWS_DEFAULT_REGION}
   aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
   aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 fi
